2ca1a9b2d4020e480cb3d549807eaaedae1e40b5
stvjbz <stvjbz@outlook.com>
2016-05-20 00:37:41 +0900

    [add] SaveConfirmDialogFragmentクラス追加.
    ダイアログでOKを選択時にデータベースへ走行距離を保存する.

--- app/src/main/java/com/stvjbz/sampleapp10/SaveConfirmDialogFragment.java
+++ app/src/main/java/com/stvjbz/sampleapp10/SaveConfirmDialogFragment.java
@@ -1,9 +1,55 @@
 package com.stvjbz.sampleapp10;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
 
 /**
  * Created by stvjbz on 16/05/18.
  */
-public class SaveConfirmDialogFragment extends DialogFragment{
+public class SaveConfirmDialogFragment extends DialogFragment {
+    private static final String ARG_TITLE = "title";
+    private static final String ARG_MESSAGE = "message";
+
+    private int mTitle;
+    private String mMessage;
+
+    public static SaveConfirmDialogFragment newInstance(int title, String message) {
+        SaveConfirmDialogFragment fragment = new SaveConfirmDialogFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_TITLE, title);
+        args.putString(ARG_MESSAGE, message);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        if (getArguments() != null) {
+            mTitle = getArguments().getInt(ARG_TITLE);
+            mMessage = getArguments().getString(ARG_MESSAGE);
+        }
+        return new AlertDialog().Builder(getActivity())
+                .setTitle(mTitle)
+                .setMessage(mMessage)
+                .setNavigativeButton(R.string.alert_dialog_cancel,
+                    new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            // do nothing
+                        }
+                    }
+                ).setPositiveButton(R.string.alert_dialog_ok,
+                       new DialogInterface.OnCancelListener() {
+                           @Override
+                           public void onClick(DialogInterface dialog, int whichButton) {
+                               //((MapsActivity) getActivity()).saveJog();
+                               ((MapsActivity) getActivity()).saveJogViaCTP();
+                           }
+                       }
+                ).create();
+    }
 }
+
