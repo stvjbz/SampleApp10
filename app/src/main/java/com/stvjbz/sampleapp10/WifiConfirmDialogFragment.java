606b88d484c5a85f9a52e20ed9bb2d45c8711a61
stvjbz <stvjbz@outlook.com>
2016-05-21 22:51:10 +0900

    [add] WillConfirmDialogFragment.

--- app/src/main/java/com/stvjbz/sampleapp10/WifiConfirmDialogFragment.java
+++ app/src/main/java/com/stvjbz/sampleapp10/WifiConfirmDialogFragment.java
@@ -5,5 +5,7 @@ import android.app.DialogFragment;
 /**
  * Created by stvjbz on 16/05/18.
  */
-public class WifiConfirmDialogFragment extends DialogFragment{
+public class WifiConfirmDialogFragment extends DialogFragment {
+    public static final String ARG_TITLE = "title";
+    private static final String ARG_MESSAGE = "message";
 }
