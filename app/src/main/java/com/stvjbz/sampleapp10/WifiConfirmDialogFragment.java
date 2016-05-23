3c6ca37e061fa57db21108cb2c578fff97ed852b
stvjbz <stvjbz@outlook.com>
2016-05-23 21:55:40 +0900

    [add] WillConfirmDialogFragment.

--- app/src/main/java/com/stvjbz/sampleapp10/WifiConfirmDialogFragment.java
+++ app/src/main/java/com/stvjbz/sampleapp10/WifiConfirmDialogFragment.java
@@ -1,6 +1,9 @@
 package com.stvjbz.sampleapp10;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 /**
@@ -20,5 +23,34 @@ public class WifiConfirmDialogFragment extends DialogFragment {
         args.putInt(ARG_MESSAGE, message);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        if (getArguments() != null) {
+            mTitle = getArguments().getInt(ARG_TITLE);
+            mMessage = getArguments().getInt(ARG_MESSAGE);
+        }
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(mTitle)
+                .setMessage(mMessage)
+                .setNegativeButton(R.string.alert_dialog_no,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // do nothing
+                            }
+                        }
+                )
+                .setPositiveButton(R.string.alert_dialog_yes,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                ((MapsActivity) getActivity()).wifiOff();
+                            }
+                        }
+                )
+                .create();
+        )
     }
 }
